e45677c841a146161c4dd0a92fc18b40902c59dc
Din Kapach <dinkapach@gmail.com>
2017-06-01 20:01:18 +0300

    add another db support

--- src/server/app.js
+++ src/server/app.js
@@ -13,8 +13,9 @@ import dashboard from './routes/dashboard';
 import club from './routes/club';
 import manager from './routes/manager';
 
-
-mongoose.connect('mongodb://localhost:27017/project', { config: { autoIndex: false } });
+const uri = mongodb://heroku_96z67r5p:d0htimb5l2h6kjguqgegaf4u4d@ds161121.mlab.com:61121/heroku_96z67r5p;
+//const uri = mongodb://localhost:27017/project;
+mongoose.connect(uri, { config: { autoIndex: false } });
 
 const app = express();
 
