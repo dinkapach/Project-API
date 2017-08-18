41bce57c7e5e912c24147aa72a1eb5f6a18775c8
Din Kapach <dinkapach@gmail.com>
2017-08-18 17:57:03 +0300

    fix login

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -15,27 +15,30 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
   const password = req.body.password;
   const email = req.body.email;
-
+  console.log("email: " + email + "pass: " + password);
   CustomerRepository.findCustomerByEmail(email)
   .then(customer => {
+    console.log(customer);
     if(customer){
       Crypto.isMatch(password, customer.password)
       .then(match => {
+        console.log(match);
           if(match) {
               console.log("customer logged in");
               res.status(200).json(customer);
           } 
           else { 
-            res.status(200).json(null);
+            res.status(400).json("wrong password");
             console.log("wrong password"); 
           }
       })
       .catch(err => { 
         console.log(err); 
+        res.status(500).json(err);
       })
     }
     else {
-      res.status(200).json(customer);
+      res.status(400).json(customer);
     }
   })
   .catch(err => {
